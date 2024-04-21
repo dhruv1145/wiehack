cfc45747fe8158c489290f15d315c84584a79115
dhruv1145 <dhruvgupta924@gmail.com>
2024-04-21 14:42:42 +0530

    first commit

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import './Content.css'
 import grp_img from '../../assets/group.jpg'
 
-function Content({ props }) {
+function Content(props) {
     return (
         <div className='main-content'>
             <div className="title-content">
-                <h1>Sports Coaching</h1>
+                <h1>{props.name}</h1>
             </div>
             <div className="sub-content">
-                <img src={grp_img} alt="" />
+                <img src={props.image} alt="" />
                 <div className="sub-right">
-                    <p>Get coaching for almost all kinds of sports from experienced and professional coaches for the age group of 3 yrs and above. Basketball is our core sport, and with our affiliation with Thane District Basketball Association, we can provide your child with maximum support to excel in the sport as a career.</p>
+                    <p>{props.text}</p>
                     <button><a href="">Enquiry</a></button>
                 </div>
 
