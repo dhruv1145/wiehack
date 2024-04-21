33833af7aeb5e77a58374edcac28ff735d7a10c0
dhruv1145 <dhruvgupta924@gmail.com>
2024-04-21 15:01:55 +0530

    first commit

--- src/components/Services/Service.jsx
+++ src/components/Services/Service.jsx
@@ -3,6 +3,8 @@ import './Service.css'
 import Content from '../Content/Content'
 import sports_img from '../../assets/sports.png'
 import grp_img from '../../assets/group.jpg'
+import league_img from '../../assets/olympics2.jpeg'
+import analysis_img from '../../assets/performance.jpeg'
 
 function Service() {
     return (
@@ -15,8 +17,8 @@ function Service() {
 
             <div className="content">
                 <Content name="Sports Coaching" text="Get coaching for almost all kinds of sports from experienced and professional coaches for the age group of 3 yrs and above. Basketball is our core sport, and with our affiliation with Thane District Basketball Association, we can provide your child with maximum support to excel in the sport as a career." image={grp_img} />
-                <Content name="Sports Coaching" text="Get coaching for almost all kinds of sports from experienced and professional coaches for the age group of 3 yrs and above. Basketball is our core sport, and with our affiliation with Thane District Basketball Association, we can provide your child with maximum support to excel in the sport as a career." image={grp_img} />
-                <Content name="Sports Coaching" text="Get coaching for almost all kinds of sports from experienced and professional coaches for the age group of 3 yrs and above. Basketball is our core sport, and with our affiliation with Thane District Basketball Association, we can provide your child with maximum support to excel in the sport as a career." image={grp_img} />
+                <Content name="Personalized Leagues" text="Get coaching for almost all kinds of sports from experienced and professional coaches for the age group of 3 yrs and above. Basketball is our core sport, and with our affiliation with Thane District Basketball Association, we can provide your child with maximum support to excel in the sport as a career." image={league_img} />
+                <Content name="Performance Analysis" text="Get coaching for almost all kinds of sports from experienced and professional coaches for the age group of 3 yrs and above. Basketball is our core sport, and with our affiliation with Thane District Basketball Association, we can provide your child with maximum support to excel in the sport as a career." image={analysis_img} />
             </div>
 
 
